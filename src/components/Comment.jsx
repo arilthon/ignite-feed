03f74df097d96b50632b5b524019921954f7443c
arilthon <arilthony@gmail.com>
2023-01-12 16:41:00 -0300

    Componente Avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,16 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css"
 
 export function Comment() {
     return (
       <div className={styles.comment}>
-        <img src="https://github.com/arilthon.png" alt="" />
+        <Avatar hasBorder={false} src="https://github.com/arilthon.png" alt="" />
 
         <div className={styles.commentBox}>
           <div className={styles.commentContent}>
             <header>
-              <div className={styles.authorAndTimes}>
+              <div className={styles.authorAndTime}>
                 <strong>Arilthon Lopes</strong>
                 <time
                   title="11 de Janeiro ás 14:22"
@@ -18,6 +19,7 @@ export function Comment() {
                   Publicado há 1h
                 </time>
               </div>
+
               <button title="Deletar comentário">
                 <Trash size={24} />
               </button>
