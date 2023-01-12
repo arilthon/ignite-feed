57aac25cd2ab5f0288743306cd642b7fed2c6a8b
arilthon <arilthony@gmail.com>
2023-01-12 14:12:52 -0300

    Componente Comment e Estilizações

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css"
+
+export function Comment() {
+    return (
+      <div className={styles.comment}>
+        <img src="https://github.com/arilthon.png" alt="" />
+
+        <div className={styles.commentBox}>
+          <div className={styles.commentContent}>
+            <header>
+              <div className={styles.authorAndTimes}>
+                <strong>Arilthon Lopes</strong>
+                <time
+                  title="11 de Janeiro ás 14:22"
+                  dateTime="2023-01-11 14-23-00"
+                >
+                  Publicado há 1h
+                </time>
+              </div>
+              <button title="Deletar comentário">
+                <Trash size={24} />
+              </button>
+            </header>
+
+            <p>Muito bom Devon, parabéns!! </p>
+          </div>
+
+          <footer>
+            <button>
+              <ThumbsUp />
+              Aplaudir <span>20</span>
+            </button>
+          </footer>
+        </div>
+      </div>
+    );
+}
